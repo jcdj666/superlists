cedaed9df0be953a59fb55c8a7ab9bbcee566f12
jcdj <2586814786@qq.com>
2022-06-07 14:38:56 +0800

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -94,24 +94,45 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers',page_text)
         self.assertIn('Buy milk',page_text)
         # Satisfied，they both go back to sleep
-
         
-# She is invited to enter a to-do item straight away
+        # She is invited to enter a to-do item straight away
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
 
-# when she hits enter, the page updates, and now the page lists
-# "1:Buy peacock feathers" as an item in a to-do list
+        # when she hits enter, the page updates, and now the page lists
+        # "1:Buy peacock feathers" as an item in a to-do list
 
-# There is still a text box inviting her to add another item, She
-# enters "Use peacock feathers to make a fly"(Edith is very methodical)
+        # There is still a text box inviting her to add another item, She
+        # enters "Use peacock feathers to make a fly"(Edith is very methodical)
 
-# The page updates again，and now shows both items on her list
+        # The page updates again，and now shows both items on her list
 
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
 
-# She visits that URL - her to-do list is still there. 
-# Satisfied，she goes back to sleep
+        # She visits that URL - her to-do list is still there. 
+        # Satisfied，she goes back to sleep
+        
+    def test_layout_and_styling(self):
+        #Edith goes to the hone page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+        #She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+
